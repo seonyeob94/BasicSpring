25fe43f247e961a1a1d1507e8357b980c45c8244
lsy <iseonyeob41@gmail.com>
2025-05-22 19:49:06 +0900

    0522수업내용 추가

--- src/main/java/kr/or/ddit/api/FirstApiController.java
+++ src/main/java/kr/or/ddit/api/FirstApiController.java
@@ -66,18 +66,20 @@ public class FirstApiController {
 
     /*
     요청URI : /articles/update
-    요청파라미터 : request{id=2,title=개똥이개똥이,content=즐거워즐거워}
+    요청파라미터 : JSON Stirng{id=2,title=개똥이개똥이,content=즐거워즐거워}
     요청방식 : post
 
     URL 요청 접수
     매개변수로 DTO 받아 오기
      */
     @PostMapping("/articles/update")
-    public String update(ArticleForm form){
+    public Article update(@RequestBody ArticleForm form){
+        //{id=2, title=개똥이의 여행22, content=즐거운 여행22}
         log.info("update->form : " + form);
         //        1. DTO를 엔티티로 변환
         //DTO(form)를 엔티티(articleEntity)로 변환
         Article articleEntity = form.toEntity();
+        //articleEntity{id=2, title=개똥이의 여행22, content=즐거운 여행22}
         log.info("update->articleEntity : " + articleEntity);
 
         //        2. 엔티티를 DB에 저장
@@ -88,10 +90,13 @@ public class FirstApiController {
         //2-2. 기존 데이터 값을 갱신하기
         //엔티티를 DB에 저장(갱신)
         if(target != null){//검증완료
+            //articleEntity{id=2, title=개똥이의 여행22, content=즐거운 여행22}
             this.articleRepository.save(articleEntity);
         }
-        //        3. 수정 결과 페이지로 리다이렉트(상세 보기) : 새로운 URI를 재요청
-        return "redirect:/articles/"+articleEntity.getId();
+
+        //   데이터
+        //articleEntity{id=2, title=개똥이의 여행22, content=즐거운 여행22}
+        return target;
     }
 
 }
--- src/main/java/kr/or/ddit/api/LprodApiController.java
+++ src/main/java/kr/or/ddit/api/LprodApiController.java
@@ -1,13 +1,11 @@
 package kr.or.ddit.api;
 
+import kr.or.ddit.dto.LprodForm;
 import kr.or.ddit.entity.Lprod;
 import kr.or.ddit.repository.LprodRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -47,5 +45,40 @@ public class LprodApiController {
         log.info("show->lprodEntity : " + lprodEntity);
         //데이터 응답(JSON String)
         return lprodEntity;
+    }
+
+    /*
+    요청URI : /lprod/update
+    요청파라미터 : JSON Stirng{id=2,title=개똥이개똥이,content=즐거워즐거워}
+    요청방식 : post
+
+    URL 요청 접수
+    매개변수로 DTO 받아 오기
+     */
+    @PostMapping("/api/lprod/update")
+    public Lprod update(@RequestBody LprodForm form){
+        //{id=2, title=개똥이의 여행22, content=즐거운 여행22}
+        log.info("update->form : " + form);
+        //        1. DTO를 엔티티로 변환
+        //DTO(form)를 엔티티(articleEntity)로 변환
+        Lprod lprodEntity = form.toEntity();
+        //articleEntity{id=2, title=개똥이의 여행22, content=즐거운 여행22}
+        log.info("update->lprodEntity : " + lprodEntity);
+
+        //        2. 엔티티를 DB에 저장
+        //2-1. DB에서 기존 데이터 가져오기(검증)
+        Lprod target = this.lprodRepository.findById(lprodEntity.getLprodId()).orElse(null);
+        log.info("update->target : " + target);
+
+        //2-2. 기존 데이터 값을 갱신하기
+        //엔티티를 DB에 저장(갱신)
+        if(target != null){//검증완료
+            //articleEntity{id=2, title=개똥이의 여행22, content=즐거운 여행22}
+            this.lprodRepository.save(lprodEntity);
+        }
+
+        //   데이터
+        //articleEntity{id=2, title=개똥이의 여행22, content=즐거운 여행22}
+        return target;
     }
 }
